3e592e0076e9fa0f5a21a1740ed3e5bb2689f4c5
Mojo Dojo <mojodojodev@gmail.com>
2023-07-11 13:15:52 +1000

    New TWIM

--- vuepress.config.ts
+++ vuepress.config.ts
@@ -145,6 +145,7 @@ export default defineUserConfig({
                         link: '/this-week-in-mojo/',
                         collapsible: true,
                         children: [
+                            '/this-week-in-mojo/2023-07-14.md',
                             '/this-week-in-mojo/2023-07-07.md',
                             '/this-week-in-mojo/2023-06-30.md',
                             '/this-week-in-mojo/2023-06-23.md',
